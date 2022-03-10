437f657553a796be020b57c190b0759df16ebbff
edoardobinda <81161437+edoardobinda@users.noreply.github.com>
2022-03-10 18:32:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+var projects = [
+    {projectName: "Tribute Page",
+    imgSrc:"images1/tribute.jpg",
+    projectLink: "https://codepen.io/edoardobinda/full/PoWrWMx",
+    sourceRepo: "https://codepen.io/edoardobinda/pen/PoWrWMx"
+    },
+    {projectName: "Survey Form",
+    imgSrc:"images1/survey.jpg",
+    projectLink: "https://codepen.io/edoardobinda/full/RwKzJXG",
+    sourceRepo: "https://codepen.io/edoardobinda/pen/RwKzJXG"
+    },
+    {projectName: "Product Landing Page",
+    imgSrc:"images1/product.jpg",
+    projectLink: "https://codepen.io/edoardobinda/full/abJoyOv",
+    sourceRepo: "https://codepen.io/edoardobinda/pen/abJoyOv"
+    },
+    {projectName: "Technical Documentation Page",
+    imgSrc:"images1/technical.jpg",
+    projectLink: "https://codepen.io/edoardobinda/full/ZEeEYoY",
+    sourceRepo: "https://codepen.io/edoardobinda/pen/ZEeEYoY"
+    },
+    {projectName: "Increase and Lower the count",
+    imgSrc:"images/addlower.jpg",
+    projectLink: "https://edoardobinda.github.io/Add-Count-Lower-Count/",
+    sourceRepo: "https://github.com/edoardobinda/Add-Count-Lower-Count"
+    },
+    {projectName: "Mortgage Calculator",
+    imgSrc:"images/mortgage.jpg",
+    projectLink: "https://edoardobinda.github.io/Mortgage_Calculator/",
+    sourceRepo: "https://github.com/edoardobinda/Mortgage_Calculator"
+    },
+    {projectName: "To Do List",
+    imgSrc:"images/todo.jpg",
+    projectLink: "https://edoardobinda.github.io/To_Do_List/",
+    sourceRepo: "https://github.com/edoardobinda/To_Do_List"
+    },
+    {projectName: "Tips Calculator",
+    imgSrc:"images/tips.jpg",
+    projectLink: "https://edoardobinda.github.io/Tips_Calculator/",
+    sourceRepo: "https://github.com/edoardobinda/Tips_Calculator"
+    },
+    {projectName: "Color Art Picker",
+    imgSrc:"images/pixel.jpg",
+    projectLink: "https://edoardobinda.github.io/Color-Art-Picker/",
+    sourceRepo: "https://github.com/edoardobinda/Color-Art-Picker"
+    }
+]
+
+
+for (let i = 0; i < projects.length; i++) {
+
+    let divProject = document.createElement("div");
+    /*divProject.setAttribute("id", "div"+[i]);*/
+    divProject.setAttribute("class", "project");
+
+    let projectTitle = document.createElement("h3");
+    projectTitle.setAttribute("id", "projectTitle");
+    projectTitle.innerHTML = projects[i].projectName;
+    console.log(projectTitle);
+
+    let projectAnchorLive = document.createElement("a");
+    projectAnchorLive.setAttribute("id", "projectAnchorImage");
+    projectAnchorLive.href = projects[i].projectLink;
+    projectAnchorLive.target = "_blank";
+
+
+    let projectImage = document.createElement("img");
+    projectImage.src = projects[i].imgSrc;
+    projectImage.setAttribute("class", "imageclass");
+
+
+    let projectAnchorRepo = document.createElement("a");
+    projectAnchorRepo.setAttribute("id", "projectAnchorRepo");
+    projectAnchorRepo.innerHTML = "Link to the Repo";
+    projectAnchorRepo.setAttribute("class", "button");
+    projectAnchorRepo.href = projects[i].sourceRepo;
+    projectAnchorRepo.target = "_blank";
+
+    //Appending childs
+    let prov1 = document.getElementById("prov1");
+    prov1.appendChild(divProject);
+    divProject.appendChild(projectTitle);
+
+    divProject.appendChild(projectAnchorLive);
+    projectAnchorLive.appendChild(projectImage);
+
+    divProject.appendChild(projectAnchorRepo);
+}
+
