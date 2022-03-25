3660d0f8d80264d03b081f291ed1ef38b620a70e
edoardobinda <81161437+edoardobinda@users.noreply.github.com>
2022-03-25 11:50:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 var projects = [
     {projectName: "Tribute Page",
-    imgSrc:"images1/tribute.jpg",
+    imgSrc:"images0/tribute.jpg",
     projectLink: "https://codepen.io/edoardobinda/full/PoWrWMx",
     sourceRepo: "https://codepen.io/edoardobinda/pen/PoWrWMx"
     },
     {projectName: "Survey Form",
-    imgSrc:"images1/survey.jpg",
+    imgSrc:"images0/survey.jpg",
     projectLink: "https://codepen.io/edoardobinda/full/RwKzJXG",
     sourceRepo: "https://codepen.io/edoardobinda/pen/RwKzJXG"
     },
     {projectName: "Product Landing Page",
-    imgSrc:"images1/product.jpg",
+    imgSrc:"images0/product.jpg",
     projectLink: "https://codepen.io/edoardobinda/full/abJoyOv",
     sourceRepo: "https://codepen.io/edoardobinda/pen/abJoyOv"
     },
     {projectName: "Technical Documentation Page",
-    imgSrc:"images1/technical.jpg",
+    imgSrc:"images0/technical.jpg",
     projectLink: "https://codepen.io/edoardobinda/full/ZEeEYoY",
     sourceRepo: "https://codepen.io/edoardobinda/pen/ZEeEYoY"
     },
     {projectName: "Increase and Lower the count",
-    imgSrc:"images/addlower.jpg",
+    imgSrc:"images0/addlower.jpg",
     projectLink: "https://edoardobinda.github.io/Add-Count-Lower-Count/",
     sourceRepo: "https://github.com/edoardobinda/Add-Count-Lower-Count"
     },
     {projectName: "Mortgage Calculator",
-    imgSrc:"images/mortgage.jpg",
+    imgSrc:"images0/mortgage.jpg",
     projectLink: "https://edoardobinda.github.io/Mortgage_Calculator/",
     sourceRepo: "https://github.com/edoardobinda/Mortgage_Calculator"
     },
     {projectName: "To Do List",
-    imgSrc:"images/todo.jpg",
+    imgSrc:"images0/todo.jpg",
     projectLink: "https://edoardobinda.github.io/To_Do_List/",
     sourceRepo: "https://github.com/edoardobinda/To_Do_List"
     },
     {projectName: "Tips Calculator",
-    imgSrc:"images/tips.jpg",
+    imgSrc:"images0/tips.jpg",
     projectLink: "https://edoardobinda.github.io/Tips_Calculator/",
     sourceRepo: "https://github.com/edoardobinda/Tips_Calculator"
     },
     {projectName: "Color Art Picker",
-    imgSrc:"images/pixel.jpg",
+    imgSrc:"images0/pixel.jpg",
     projectLink: "https://edoardobinda.github.io/Color-Art-Picker/",
     sourceRepo: "https://github.com/edoardobinda/Color-Art-Picker"
     }
@@ -71,7 +71,7 @@ for (let i = 0; i < projects.length; i++) {
 
     let projectAnchorRepo = document.createElement("a");
     projectAnchorRepo.setAttribute("id", "projectAnchorRepo");
-    projectAnchorRepo.innerHTML = "Link to the Repo";
+    projectAnchorRepo.innerHTML = "Link Repository";
     projectAnchorRepo.setAttribute("class", "button");
     projectAnchorRepo.href = projects[i].sourceRepo;
     projectAnchorRepo.target = "_blank";
